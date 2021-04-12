913cb0af8046c0cd99fde793d3e84d2461797995
glendo <gleneggers64@gmail.com>
2021-04-12 17:04:06 -0500

    initial commit; dummy employee object for generating HTML

---
+++ src/Dummy.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+        name: "Glen",
+        id: 0,
+        email: "glen@glen.com",
+        role: "Manager",
+        officeNumber: 42
+    },
+    {
+        name: "Tanner",
+        id: 1,
+        email: "tanner@tanner.com",
+        role: "Engineer",
+        github: "ToastKitten"
+    },
+    {
+        name: "Emily",
+        id: 2,
+        email: "emily@emily.com",
+        role: "Intern",
+        school: "University of Texas at Austin"
+    },
+    {
+        name: "Alicia",
+        id: 3,
+        email: "alicia@alicia.com",
+        role: "Intern",
+        school: "CodeCademy"
+    }
+];
