5987170c7f9d47fc0aa18334efc1b3b37933f11f
glendo <gleneggers64@gmail.com>
2021-04-14 11:26:05 -0500

    revise require generate-site; add promise handling for app logic

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-const generateSite = require("./utils/generate-site.js")
+const { writeFile, copyFile } = require("./utils/generate-site.js")
 const generatePage = require("./src/page-template.js");
-const Dummy = require("./src/Dummy.js");
-const getQuestions = require("./src/questions.js");
 const Team = require("./lib/Team.js");
 
-async function createTeam() {
-    const team = new Team();
-    let employeeData = await team.initializeTeam();
-    console.log(employeeData);
-    return employeeData;
-}
-
-createTeam()
-// call prompt to get user input about employees
-
-// call page template with employees array to generate html
-
-// call generate-site to write a file with html ti dist and to copy css file to dist
+new Team().initializeTeam()
+    .then(data => writeFile(generatePage(data)))
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => console.log(copyFileResponse))
+    .catch(err => console.log(err));
