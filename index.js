b99ce009f4ece7c4e4691b2f32c34ab4b2e6d1ec
glendo <gleneggers64@gmail.com>
2021-04-15 13:36:01 -0500

    add descriptive end of program log; change logs to only log response messages

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const Team = require("./lib/Team.js");
 new Team().initializeTeam()
     .then(data => writeFile(generatePage(data)))
     .then(writeFileResponse => {
-        console.log(writeFileResponse);
+        console.log(writeFileResponse.message);
         return copyFile();
     })
-    .then(copyFileResponse => console.log(copyFileResponse))
+    .then(copyFileResponse => {
+        console.log(copyFileResponse.message)
+        console.log("Check out your team profile by opening the index.html file in the /dist folder in your browser!")
+    })
     .catch(err => console.log(err));
